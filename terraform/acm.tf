1860b7dc86553772ff42e32bc0abf14dac19c415
Skillz008 <jossylajide4grace301@gmail.com>
2025-09-21 10:47:51 +0100

    Update acm.tf

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -14,23 +14,23 @@ data "aws_route53_zone" "selected" {
   }
 }
 
-# Create a DNS record for ACM validation
-resource "aws_route53_record" "acm_validation" {
-  for_each = {
-    for dvo in aws_acm_certificate.main.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
-
-  allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = 60
-  type            = each.value.type
-  zone_id         = data.aws_route53_zone.selected.zone_id
-}
+# In acm.tf, comment out the problematic resource:
+# resource "aws_route53_record" "acm_validation" {
+#   for_each = {
+#     for dvo in aws_acm_certificate.main.domain_validation_options : dvo.domain_name => {
+#       name   = dvo.resource_record_name
+#       record = dvo.resource_record_value
+#       type   = dvo.resource_record_type
+#     }
+#   }
+# 
+#   allow_overwrite = true
+#   name            = each.value.name
+#   records         = [each.value.record]
+#   ttl             = 60
+#   type            = each.value.type
+#   zone_id         = data.aws_route53_zone.selected.zone_id
+# }
 
 # Create an ALIAS record in Route53 pointing to the ALB
 resource "aws_route53_record" "main" {
