b123cab50e762e6632aaa55696ce87560af28ab8
Skillz008 <jossylajide4grace301@gmail.com>
2025-09-21 09:38:26 +0100

    Update acm.tf

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -1,20 +1,13 @@
 resource "aws_acm_certificate" "main" {
-  domain_name       = "*.innovatemart.project.com"
+  domain_name       = "*.Skillz008.onmicrosoft.com"
   validation_method = "DNS"
 }
 
-# Use data source to get the Route53 zone
 data "aws_route53_zone" "selected" {
-  name         = aws_route53_zone.main.name
+  name         = "Skillz008.onmicrosoft.com." # Use the root domain
   private_zone = false
-  # Add these to ensure only one match
-  vpc_id       = null # Ensure it's a public hosted zone
-  tags = {
-    Project = var.project_name
-  }
 }
 
-# Create a DNS record for ACM validation
 resource "aws_route53_record" "acm_validation" {
   for_each = {
     for dvo in aws_acm_certificate.main.domain_validation_options : dvo.domain_name => {
@@ -32,16 +25,7 @@ resource "aws_route53_record" "acm_validation" {
   zone_id         = data.aws_route53_zone.selected.zone_id
 }
 
-# Create an ALIAS record in Route53 pointing to the ALB
-resource "aws_route53_record" "main" {
-  zone_id = data.aws_route53_zone.selected.zone_id
-  name    = "innovatemart.project.com"
-  type    = "A"
-
-  alias {
-  # name                   = kubernetes_ingress_v1.retail_store_ingress.status.0.load_balancer.0.ingress.0.hostname
-    name		   = "your-alb-dns-name"
-    zone_id                = aws_route53_zone.main.zone_id
-    evaluate_target_health = true
-  }
+resource "aws_acm_certificate_validation" "main" {
+  certificate_arn         = aws_acm_certificate.main.arn
+  validation_record_fqdns = [for record in aws_route53_record.acm_validation : record.fqdn]
 }
